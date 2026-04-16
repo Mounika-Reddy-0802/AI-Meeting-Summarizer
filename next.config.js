b1088b45ef0cb66d0163007acb01d677be10c7fe
Shery Mounika Reddy <sherymounikareddy.2006@gmail.com>
2026-04-16 13:06:56 +0530

    Expose Zoom env vars to SSR runtime

--- next.config.js
+++ next.config.js
@@ -11,4 +11,11 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    ZOOM_SDK_KEY: process.env.ZOOM_SDK_KEY,
+    ZOOM_SDK_SECRET: process.env.ZOOM_SDK_SECRET,
+    ZOOM_ACCOUNT_ID: process.env.ZOOM_ACCOUNT_ID,
+    ZOOM_S2S_CLIENT_ID: process.env.ZOOM_S2S_CLIENT_ID,
+    ZOOM_S2S_CLIENT_SECRET: process.env.ZOOM_S2S_CLIENT_SECRET,
+  },
 };
